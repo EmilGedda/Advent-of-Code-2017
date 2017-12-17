c792cb780a33a1ec765cf09b16858663a7d648e8
Emil Gedda <emil.gedda@emilgedda.se>
2017-12-16 18:59:24 -0600

    day 3 part one done

---
+++ src/3_1.cpp
@@ -0,0 +1,19 @@
+#include <advent-of-code.hpp>
+#include <cmath>
+#include <iostream>
+#include <numeric>
+
+int main() {
+  int pos;
+  std::cin >> pos;
+  int sqr = std::ceil(std::sqrt(pos));
+  if ((sqr & 1) == 0)
+    sqr++; // only allow odd sizes
+
+  const int min = sqr >> 1; // minimum amount of travel distance
+  const int period = sqr - 1; // period of the extra distance from the center
+  const int offset = (sqr * sqr - pos) % period; // center of axis offset
+  const int dist = std::abs(offset - period / 2) + min; // cummulative distance
+
+  std::cout << dist << '\n';
+}
