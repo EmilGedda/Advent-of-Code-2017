466933ab99a23371c22980896f9e55c2c9dc2a80
Emil Gedda <emil.gedda@emilgedda.se>
2017-12-17 21:33:21 -0600

    day 4 part one and two done

---
+++ src/4_2.cpp
@@ -0,0 +1,24 @@
+#include <advent-of-code.hpp>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+int main() {
+
+  std::string line;
+  int valid = 0;
+  while (std::getline(std::cin, line)) {
+    std::stringstream ss(line);
+    std::string word;
+    std::unordered_map<std::string, bool> not_a_bloom_filter;
+    bool inserted = true;
+    while (ss >> word && inserted) {
+      std::sort(word.begin(), word.end());
+      inserted = not_a_bloom_filter.emplace(word, true).second;
+    }
+    valid += inserted;
+  }
+  std::cout << valid << '\n';
+}
